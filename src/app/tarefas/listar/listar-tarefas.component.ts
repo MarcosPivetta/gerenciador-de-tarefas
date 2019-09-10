5099cdebda5f8f98d0c5abd4901cab7f71b96818
MarcosPivetta <marcos.pivetta@hotmail.com.br>
2019-09-10 19:54:19 -0300

    67. Alterando o status de uma tarefa

--- src/app/tarefas/listar/listar-tarefas.component.ts
+++ src/app/tarefas/listar/listar-tarefas.component.ts
@@ -23,16 +23,16 @@ export class ListarTarefasComponent implements OnInit {
 
   remover($event: any, tarefa: Tarefa): void {
     $event.preventDefault();
-    if (confirm('Deseja remover tarefa "' + tarefa.nome + '" ?')) {
+    if (confirm('Deseja remover a tarefa "' + tarefa.nome + '"?')) {
       this.tarefaService.remover(tarefa.id);
-      this.tarefas = this.tarefaService.listarTodos();
+      this.tarefas = this.listarTodos();
     }
   }
 
   alterarStatus(tarefa: Tarefa): void {
-    if (confirm('Deseja alterar o status da tarefa "' + tarefa.nome + '" ?')) {
+    if (confirm('Deseja alterar o status da tarefa "' + tarefa.nome + '"?')) {
       this.tarefaService.alterarStatus(tarefa.id);
-      this.tarefas = this.tarefaService.listarTodos();
+      this.tarefas = this.listarTodos();
     }
   }
 }
